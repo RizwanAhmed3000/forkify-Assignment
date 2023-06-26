e72a61907faf4bf66ddf2d299d417e141a49e746
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-06-26 20:31:38 +0500

    single product data into ui

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const itemListContainer = document.querySelector('.left')
 const searchBar = document.querySelector('.searchBar')
 const searchBtn = document.querySelector('.searchBtn')
+const productDetail = document.querySelector('.productDetail')
 
 
 function fetchRecipeList(value) {
@@ -31,18 +32,75 @@ function mappingData(data) {
     itemListContainer.innerHTML = dataIntoUi
 }
 
-function filteringData(){
+function singleItemDataInUi(data) {
+    const productObjPath = data.data.recipe
+    const product = `<div class="card mb-3">
+    <div class="img">
+        <img src=${productObjPath.image_url}
+            class="card-img-top image-fit" alt="Recipe Image">
+    </div>
+    <div class="card-body d-flex flex-column align-items-center">
+        <h1 class="card-title text-center recipeName">${productObjPath.title}</h1>
+        <div class="container d-flex justify-content-center info">
+            <div class="d-flex me-auto justify-content-center align-items-center">
+                <p id="timeToCook"><i class="fa-regular fa-clock me-2"></i>${productObjPath.cooking_time} minutes</p>
+                <p id="servings"><i class="fa-solid fa-user-group me-2"></i>${productObjPath.servings} servings</p>
+            </div>
+            <p id="bookmark"><i class="fa-solid fa-bookmark rounded-circle p-3 fs-5" style="color: #ffffff;"></i></p>
+        </div>
+    </div>
+</div>
+
+<div class="card ingredientCard mb-3" style="width: 100%; height: 400px;">
+                    <div class="card-body d-flex flex-column flex-wrap">
+                        <h4 class="card-title text-center my-4 recipeIng">Recipe Ingredients</h4>
+                        <div id="ingredientsList" class="d-flex flex-column flex-wrap">
+                            ${ingredients(productObjPath.ingredients)}
+                        </div>
+                    </div>
+                </div>
+
+<div class="card mb-3">
+    <div class="card-body">
+        <h4 class="card-title text-center my-4 howToCook">How To Cook It</h4>
+        <p class="card-text text-center">
+            The source URL for the recipe is: 
+            
+            <a href=${productObjPath.source_url}>${productObjPath.source_url}</a>.
+        </p>
+        
+    </div>
+</div>`
+
+    productDetail.innerHTML = product;
+
+}
+
+function ingredients(ingredients) {
+    const dataToUi = ingredients.map((obj) => {
+        const list = `<div class="items d-flex align-items-baseline">
+                    <i class="fa-solid fa-check me-2"></i>
+                    <p>${obj.quantity} ${obj.unit} ${obj.description}</p>
+                </div>`
+
+        return list
+    })
+    return dataToUi
+}
+
+function filteringData() {
     const searchItem = searchBar.value;
     fetchRecipeList(searchItem);
     searchBar.value = "";
 }
 
-function getData(id){
+function getData(id) {
     const fetchData = fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     fetchData
         .then((response) => response.json())
-        .then((data)=>{
+        .then((data) => {
             console.log(data)
+            singleItemDataInUi(data)
         })
 }
 
